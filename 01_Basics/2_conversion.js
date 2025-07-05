2e527dbc736847d6762372c57a6d4830639f109e
acer <sachinkumar270702@gmail.com>
2025-07-05 13:34:03 +0530

    type conversion in js

---
+++ 01_Basics/2_conversion.js
@@ -0,0 +1,25 @@
+let score=null
+// console.log(typeof score) 
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=> NaN
+//true=>1;false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggerIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggerIn);
+
+//1=>true; 0=>false
+//""=> false
+//"sdabjn"(a non empty String)=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
