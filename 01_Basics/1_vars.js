e5cfd6a0cfbef7a73c246d0f9bdcef98ad2112f2
acer <sachinkumar270702@gmail.com>
2025-07-05 13:33:38 +0530

    variables in js

---
+++ 01_Basics/1_vars.js
@@ -0,0 +1,9 @@
+const accountId=1445533
+let accountEmail="Sachin@gmail.com"
+var accountPassword="12344"
+accountCity="Jaipur"
+
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
